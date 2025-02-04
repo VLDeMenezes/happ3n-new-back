a8fdf18e9b80fc92f7a565e35390f6636f00f1ac
happ3nhub <happ3nhub@gmail.com>
2025-02-04 14:25:39 -0300

    Add Event entity to EventosModule imports for improved database integration

--- src/eventos/eventos.module.ts
+++ src/eventos/eventos.module.ts
@@ -4,6 +4,7 @@ import { EventosService } from './eventos.service';
 import { MetricsModule } from 'src/metrics/metrics.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CloudinaryModule } from 'src/cloudinary/cloudinary.module';
+import { Event } from 'src/entities/event.entities';
 
 @Module({
   imports: [MetricsModule, CloudinaryModule, TypeOrmModule.forFeature([Event])],
